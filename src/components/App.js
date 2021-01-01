6223eccd5908715c325d357fa6ef89780b00d037
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-01 14:28:38 +0000

    set up editor component props and editor control

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,7 @@ function App() {
       <div className="pane">
         <iframe 
           title="output"
-          sandbox="allow-scripts"
-          // prevents codepen application accessing document cookies etc if saving code and sharing with someone else
+          sandbox="allow-scripts" // prevents codepen application accessing document cookies etc if saving code and sharing with someone else
           frameborder="0"
           width="100%"
           height="100%"

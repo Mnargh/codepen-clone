2d4a253af57a159281dfca125c27773070d4fed7
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-02 16:49:42 +0000

    Added expand collapse icon to editors

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,6 +7,8 @@ import 'codemirror/mode/xml/xml' // functionally same as html
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor( props ) {
   const {
@@ -26,9 +28,13 @@ export default function Editor( props ) {
     <div className={`editor-container ${open ? `` : `collapsed`}`}>
       <div className="editor-title">
         {displayName}
-        <button
-          onClick={() => setOpen(prevOpen => !prevOpen)}
-          >O/C</button>
+        <button 
+          type='button'
+          className='expand-collapse-btn'
+          onClick={() => setOpen(prevOpen => !prevOpen)}>
+          
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor 
         onBeforeChange={handleChange}

3f7348e46b39e7c60f2b8c9240ca7d61b142752f
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-02 17:27:01 +0000

    Created custom hook for retrieving and setting Editor state in local storage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+const PREFIX = 'codepen-clone-'
+// to show in localstorage which correspond to this app
+
+export default function useLocalStorage( key, initialValue) {
+
+  const prefixedkey = PREFIX + key
+
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedkey)
+
+    if (jsonValue != null) return JSON.parse(jsonValue)
+
+    if (typeof initialValue === 'function') {
+      return initialValue()
+    } else { return initialValue }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(prefixedkey, JSON.stringify(value))
+  }, [prefixedkey, value])
+
+  return [ value, setValue ]
+}
